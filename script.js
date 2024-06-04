2e0c1d6f21edc4defc9271e769af3e6f550f32e2
madhubala54298 <163556432+madhubala54298@users.noreply.github.com>
2024-06-04 11:45:16 +0530

    online_quiz_maker
    online_quiz_maker

--- script.js
+++ script.js
@@ -1,32 +1,217 @@
+const progressBar = document.querySelector(".progress-bar"),
+  progressText = document.querySelector(".progress-text");
 
-var l=1;
-function expandGrid(){
-    var container = document.getElementById("griid");
-    if(l==1)
-    {
-        container.innerHTML+= '<div class="box"><a href="https://en.wikipedia.org/wiki/Audi"><img class="crs" src="data/audi.jpg" alt="audi"><div class="tover"><br>AUDI</div></a></div><div class="box"><a href="https://en.wikipedia.org/wiki/BMW"><img class="crs" src="data/bmw.jpg" alt="bmw"><div class="tover"><br>BMW</div></a></div><div class="box"><a href="https://en.wikipedia.org/wiki/Mahindra_Bolero"><img class="crs" src="data/bolero.jpg" alt="bolero"><div class="tover"><br>BOLERO</div></a></div>';
-        l+=1;
+const progress = (value) => {
+  const percentage = (value / time) * 100;
+  progressBar.style.width = `${percentage}%`;
+  progressText.innerHTML = `${value}`;
+};
+
+const startBtn = document.querySelector(".start"),
+  numQuestions = document.querySelector("#num-questions"),
+  category = document.querySelector("#category"),
+  difficulty = document.querySelector("#difficulty"),
+  timePerQuestion = document.querySelector("#time"),
+  quiz = document.querySelector(".quiz"),
+  startScreen = document.querySelector(".start-screen");
+
+let questions = [],
+  time = 30,
+  score = 0,
+  currentQuestion,
+  timer;
+
+const startQuiz = () => {
+  const num = numQuestions.value,
+    cat = category.value,
+    diff = difficulty.value;
+  loadingAnimation();
+  const url = `https://opentdb.com/api.php?amount=${num}&category=${cat}&difficulty=${diff}&type=multiple`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      questions = data.results;
+      setTimeout(() => {
+        startScreen.classList.add("hide");
+        quiz.classList.remove("hide");
+        currentQuestion = 1;
+        showQuestion(questions[0]);
+      }, 1000);
+    });
+};
+
+startBtn.addEventListener("click", startQuiz);
+
+const showQuestion = (question) => {
+  const questionText = document.querySelector(".question"),
+    answersWrapper = document.querySelector(".answer-wrapper");
+  questionNumber = document.querySelector(".number");
+
+  questionText.innerHTML = question.question;
+
+  const answers = [
+    ...question.incorrect_answers,
+    question.correct_answer.toString(),
+  ];
+  answersWrapper.innerHTML = "";
+  answers.sort(() => Math.random() - 0.5);
+  answers.forEach((answer) => {
+    answersWrapper.innerHTML += `
+                  <div class="answer ">
+            <span class="text">${answer}</span>
+            <span class="checkbox">
+              <i class="fas fa-check"></i>
+            </span>
+          </div>
+        `;
+  });
+
+  questionNumber.innerHTML = ` Question <span class="current">${
+    questions.indexOf(question) + 1
+  }</span>
+            <span class="total">/${questions.length}</span>`;
+  //add event listener to each answer
+  const answersDiv = document.querySelectorAll(".answer");
+  answersDiv.forEach((answer) => {
+    answer.addEventListener("click", () => {
+      if (!answer.classList.contains("checked")) {
+        answersDiv.forEach((answer) => {
+          answer.classList.remove("selected");
+        });
+        answer.classList.add("selected");
+        submitBtn.disabled = false;
+      }
+    });
+  });
+
+  time = timePerQuestion.value;
+  startTimer(time);
+};
+
+const startTimer = (time) => {
+  timer = setInterval(() => {
+    if (time === 3) {
+      playAdudio("countdown.mp3");
     }
-    else if(l==2)
-    {        
-        container.innerHTML+= '<div class="box"><a href="https://en.wikipedia.org/wiki/Dacia_Duster"><img class="crs" src="data/duster.jpg" alt="duster"><div class="tover"><br>DUSTER</div></a></div><div class="box"><a href="https://en.wikipedia.org/wiki/Ferrari"><img class="crs" src="data/ferrai.jpg" alt="ferrari"><div class="tover"><br>FERRARI</div></a></div><div class="box"><a href="https://en.wikipedia.org/wiki/Toyota_Fortuner"><img class="crs" src="data/fortuner.jpg" alt="fortuner"><div class="tover"><br>FORTUNER</div></a></div>';
-        l+=1;
+    if (time >= 0) {
+      progress(time);
+      time--;
+    } else {
+      checkAnswer();
     }
-    else if(l==3)
-    {
-        container.innerHTML+= '<div class="box"><a href="https://en.wikipedia.org/wiki/MG_Motor"><img class="crs" src="data/hector.jpg" alt="hector"><div class="tover"><br>HECTOR</div></a></div><div class="box"><a href="https://en.wikipedia.org/wiki/Jaguar_Cars"><img class="crs" src="data/jaguar.jpg" alt="jaguar"><div class="tover"><br>JAGUAR</div></a></div><div class="box"><a href="https://en.wikipedia.org/wiki/Mercedes-Benz"><img class="crs" src="data/mercedes.jpg" alt="mercedes"><div class="tover"><br>MERCEDES</div></a></div>';
-        l+=1;
+  }, 1000);
+};
+
+const loadingAnimation = () => {
+  startBtn.innerHTML = "Loading";
+  const loadingInterval = setInterval(() => {
+    if (startBtn.innerHTML.length === 10) {
+      startBtn.innerHTML = "Loading";
+    } else {
+      startBtn.innerHTML += ".";
     }
-    else if(l==4)
-    {
-        container.innerHTML+= '<div class="box"><a href="https://en.wikipedia.org/wiki/Mahindra_Scorpio"><img class="crs" src="data/scorpio.jpg" alt="scorpio"><div class="tover"><br>SCORPIO</div></a></div>            <div class="box"><a href="https://en.wikipedia.org/wiki/Mahindra_Thar"><img class="crs" src="data/thar.jpg" alt="thar"><div class="tover"><br>THAR</div></a></div>            <div class="box"><a href="https://en.wikipedia.org/wiki/Mahindra_XUV500"><img class="crs" src="data/xuv.jpg" alt="xuv"><div class="tover"><br>XUV</div></a></div>';
-        l+=1;
-    }
-    else
-    {
-        alert('More COntent will be added Soon. Stay Tuned:)');
-    }
+  }, 500);
+};
+function defineProperty() {
+  var osccred = document.createElement("div");
+  osccred.innerHTML =
+    "A Project By <a href='https://www.youtube.com/@opensourcecoding' target=_blank>Open Source Coding</a>";
+  osccred.style.position = "absolute";
+  osccred.style.bottom = "0";
+  osccred.style.right = "0";
+  osccred.style.fontSize = "10px";
+  osccred.style.color = "#ccc";
+  osccred.style.fontFamily = "sans-serif";
+  osccred.style.padding = "5px";
+  osccred.style.background = "#fff";
+  osccred.style.borderTopLeftRadius = "5px";
+  osccred.style.borderBottomRightRadius = "5px";
+  osccred.style.boxShadow = "0 0 5px #ccc";
+  document.body.appendChild(osccred);
 }
-function sub(){
-    alert('Form Sent')
-}
+
+defineProperty();
+
+const submitBtn = document.querySelector(".submit"),
+  nextBtn = document.querySelector(".next");
+submitBtn.addEventListener("click", () => {
+  checkAnswer();
+});
+
+nextBtn.addEventListener("click", () => {
+  nextQuestion();
+  submitBtn.style.display = "block";
+  nextBtn.style.display = "none";
+});
+
+const checkAnswer = () => {
+  clearInterval(timer);
+  const selectedAnswer = document.querySelector(".answer.selected");
+  if (selectedAnswer) {
+    const answer = selectedAnswer.querySelector(".text").innerHTML;
+    console.log(currentQuestion);
+    if (answer === questions[currentQuestion - 1].correct_answer) {
+      score++;
+      selectedAnswer.classList.add("correct");
+    } else {
+      selectedAnswer.classList.add("wrong");
+      const correctAnswer = document
+        .querySelectorAll(".answer")
+        .forEach((answer) => {
+          if (
+            answer.querySelector(".text").innerHTML ===
+            questions[currentQuestion - 1].correct_answer
+          ) {
+            answer.classList.add("correct");
+          }
+        });
+    }
+  } else {
+    const correctAnswer = document
+      .querySelectorAll(".answer")
+      .forEach((answer) => {
+        if (
+          answer.querySelector(".text").innerHTML ===
+          questions[currentQuestion - 1].correct_answer
+        ) {
+          answer.classList.add("correct");
+        }
+      });
+  }
+  const answersDiv = document.querySelectorAll(".answer");
+  answersDiv.forEach((answer) => {
+    answer.classList.add("checked");
+  });
+
+  submitBtn.style.display = "none";
+  nextBtn.style.display = "block";
+};
+
+const nextQuestion = () => {
+  if (currentQuestion < questions.length) {
+    currentQuestion++;
+    showQuestion(questions[currentQuestion - 1]);
+  } else {
+    showScore();
+  }
+};
+
+const endScreen = document.querySelector(".end-screen"),
+  finalScore = document.querySelector(".final-score"),
+  totalScore = document.querySelector(".total-score");
+const showScore = () => {
+  endScreen.classList.remove("hide");
+  quiz.classList.add("hide");
+  finalScore.innerHTML = score;
+  totalScore.innerHTML = `/ ${questions.length}`;
+};
+
+const restartBtn = document.querySelector(".restart");
+restartBtn.addEventListener("click", () => {
+  window.location.reload();
+});
+
+const playAdudio = (src) => {
+  const audio = new Audio(src);
+  audio.play();
+};
